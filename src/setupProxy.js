6ec71fa209bf673838725b72d70d0f2e2a9fe11d
Enrique Guerrero <enriqueguefer@gmail.com>
2020-01-05 20:41:05 +0100

    Se añade el proxy para auth y la etiqueta changeOrigin true

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,17 +4,21 @@ const proxy = require('http-proxy-middleware');
 const API_TRANSFERS_URL = (process.env.TRANSFERS_URL || 'http://localhost:3000');
 const API_TEAMS_PLAYER_URL = (process.env.TEAMS_URL || 'http://localhost:3001');
 const API_TOURNAMENTS_URL = (process.env.TOURNAMENT_URL || 'http://localhost:3002');
+const API_AUTH_URL = (process.env.AUTH_URL || 'http://localhost:3003');
 
 module.exports = function(app) {
-  app.use(proxy('/api/v1/transfers', {target: API_TRANSFERS_URL}));
-  app.use(proxy('/api/v1/transfer/', {target: API_TRANSFERS_URL}));
-  app.use(proxy('/api/v1/transfer', {target: API_TRANSFERS_URL}));
-  app.use(proxy('/api/v1/teams', {target: API_TEAMS_PLAYER_URL}));
-  app.use(proxy('/api/v1/players', {target: API_TEAMS_PLAYER_URL}));
+  app.use(proxy('/api/v1/transfers', {target: API_TRANSFERS_URL, changeOrigin: true}));
+  app.use(proxy('/api/v1/transfer/', {target: API_TRANSFERS_URL, changeOrigin: true}));
+  app.use(proxy('/api/v1/transfer', {target: API_TRANSFERS_URL, changeOrigin: true}));
+  app.use(proxy('/api/v1/teams', {target: API_TEAMS_PLAYER_URL, changeOrigin: true}));
+  app.use(proxy('/api/v1/players', {target: API_TEAMS_PLAYER_URL, changeOrigin: true}));
 
 
   // ========= TOURNAMENTS ============
-  app.use(proxy('/api/v1/matches', {target: API_TOURNAMENTS_URL}));
-  app.use(proxy('/api/v1/matches/', {target: API_TOURNAMENTS_URL}));
-  app.use(proxy('/api/v1/match/', {target: API_TOURNAMENTS_URL}));
+  app.use(proxy('/api/v1/matches', {target: API_TOURNAMENTS_URL, changeOrigin: true}));
+  app.use(proxy('/api/v1/matches/', {target: API_TOURNAMENTS_URL, changeOrigin: true}));
+  app.use(proxy('/api/v1/match/', {target: API_TOURNAMENTS_URL, changeOrigin: true}));
+
+  // ========= AUTH ============
+  app.use(proxy('/api/v1/users/authenticate', {target: API_AUTH_URL, changeOrigin: true}));
 };
